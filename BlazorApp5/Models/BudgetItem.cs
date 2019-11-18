27f42bbe027df2e38e97b3a5064792fbf97c1423
Leger DJIBA <l.djiba@afdb.org>
2019-11-18 11:20:18 +0000

    First commit

---
+++ BlazorApp5/Models/BudgetItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp5.Models
+{
+    public class BudgetItem
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Remaining { get; set; }
+    }
+}
